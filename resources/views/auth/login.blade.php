a924e0749a3dc8219b3daa8970057c9f2a740ade
senryo0909 <19870909takuya@gmail.com>
2020-05-06 21:46:58 +0800

    TOPページの作成途中

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>login</title>
+        <style>
+
+        </style>
+</head>
+<body>
+<div style="text-center">
+<h1>ログイン</h1>
+</div>
+<p>ここにログインフォームを記載</p>
+</body>
+<html>
+
+    
