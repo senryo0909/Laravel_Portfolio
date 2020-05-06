f427dff530e0a7f579c83a2ac2b3e64db2c37b2a
senryo0909 <19870909takuya@gmail.com>
2020-05-06 18:50:59 +0800

    userテーブルの入力可能個所を変更

--- app/User.php
+++ app/User.php
@@ -16,7 +16,8 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+     'email', 'password', 'first_name', 'last_name', 'first_name_kana', 'last_name_kana',
+     'tel'
     ];
 
     /**
@@ -25,7 +26,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'password', 'remember_token', 'tel'
     ];
 
     /**
