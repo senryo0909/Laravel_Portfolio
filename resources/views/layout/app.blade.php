1b547103237f02883670613dd4528d1a00c834b6
senryo0909 <19870909takuya@gmail.com>
2020-05-11 21:48:25 +0800

    親レイアウト(app)の作成

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -10,10 +10,15 @@
     <title>@yield('title')</title>
   </head>
   <body>
-    <header>
-    </header>
+      
+    @include('inc.sidebar-file')
 
-    @yield('content')
+    <div class="container">
+            @yield('content')
+    </div>
+    
+    @include('inc.footer-file')
+    
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
