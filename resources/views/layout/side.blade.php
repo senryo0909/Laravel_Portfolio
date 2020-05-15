0e7635d7213d92fff01f6da591267383d7f16c7c
senryo0909 <19870909takuya@gmail.com>
2020-05-15 17:25:43 +0800

    サイドバーレイアウト調整・shift.blade.phpのアラートの実装

--- resources/views/layout/side.blade.php
+++ resources/views/layout/side.blade.php
@@ -1,4 +1,4 @@
-@section('side')
+
 <div class="list-group-item position-fixed ml-1 mt-1"  style="max-width: 200px;">
     <a href="#" class="list-group-item active">勤怠入力管理</a>
     <a href="#" class="list-group-item">Task管理</a>
@@ -6,5 +6,5 @@
     <a href="#" class="list-group-item">ドキュメント管理</a>
     <a href="#" class="list-group-item">会議室予約管理</a> 
 </div>
-@endsection
+
 
