e520ad63efc8f862b0d58fbd47e93c7a634e9033
senryo0909 <19870909takuya@gmail.com>
2020-05-26 11:35:42 +0800

    ログイン・ユーザー新規登録完了時のredirect先の指定。 指定先：shifts.index

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/shifts.index';
 
     /**
      * Create a new controller instance.
--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -28,7 +28,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/shifts.index';
 
     /**
      * Create a new controller instance.
